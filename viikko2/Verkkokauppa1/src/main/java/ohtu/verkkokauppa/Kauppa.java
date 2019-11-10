d3de4f535914a80a2fdf03fc2f494b4ecc639bc3
Fanni Franssila <fanni.franssila@gmail.com>
2019-11-10 17:58:42 +0200

    Tehtiin vko 2 t. 12

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -1,5 +1,9 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Kauppa {
 
     private VarastoOlio varasto;
@@ -8,6 +12,7 @@ public class Kauppa {
     private GeneraattoriOlio viitegeneraattori;
     private String kaupanTili;
 
+    @Autowired
     public Kauppa(VarastoOlio varasto, PankkiOlio pankki, GeneraattoriOlio viitegeneraattori) {
         this.varasto = varasto;
         this.pankki = pankki;
