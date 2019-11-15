e4d2e9a19e99e8c6651bbb543021ef00b0e97902
Fanni Franssila <fanni.franssila@gmail.com>
2019-11-15 17:59:46 +0200

    Vko 3 t. 8: Lisättiin läpikäytäväksi uudet skenaariot ja katsottiin, että toimi

--- viikko3/tehtava7/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/tehtava7/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -28,6 +29,65 @@ public class Tester {
         sleep(2);
         element.submit();
 
+        sleep(3);
+        
+        // Kirjaudutaan ulos seuraavaa testiä varten
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        
+        sleep(2);
+        
+        // Testataan, ettei voi kirjautua väärällä salasanalla
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("login"));
+        
+        sleep(2);
+        element.submit();
+        
+        sleep(2);
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+
+        sleep(3);      
+        
+        // Tilanne, jossa luodaan uusi käyttäjätunnus
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        
+        sleep(2);
+        
+        Random r = new Random();
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka" + r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("pekka");
+        
+        sleep(2);
+        element.submit();
+
+        sleep(2);
+        
+        //Tilanne, jossa kirjaudutaan ulos uuden käyttäjän luomisen jälkeen.
+        //Äsken juuri luotiin käyttäjä, joten jatketaan siitä suoraan.
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+        sleep(2);
+        
+        element = driver.findElement(By.linkText("logout"));
+        element.click();        
+        
         sleep(3);
         
         driver.quit();
