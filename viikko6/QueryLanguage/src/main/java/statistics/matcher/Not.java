d3ae1ff880c0e5fa4afa240464b62a465a39b8df
Fanni Franssila <fanni.franssila@gmail.com>
2019-12-06 22:12:42 +0200

    Vko 6 t.2: Toteutettiin halutut metodit

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/Not.java
@@ -0,0 +1,22 @@
+
+package statistics.matcher;
+
+import java.lang.reflect.Method;
+import statistics.Player;
+
+
+public class Not implements Matcher {
+    
+    private Matcher matcher;
+
+    public Not(Matcher matcher) {
+        this.matcher = matcher;
+    }
+
+    @Override
+    public boolean matches(Player p) {
+        return !matcher.matches(p);     
+        
+    }
+    
+}
